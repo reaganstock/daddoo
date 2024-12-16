f23a24ca9785b63f4981c3952ed9576229331845
Reagan Stock <reagan@leadmechanics.com>
2024-12-16 03:55:36 -0600

    chore: add netlify adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server'
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: netlify(),
 });
